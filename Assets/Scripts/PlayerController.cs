de8d4e29a606a117d29355c4953f176d0365e243
reiya <t.reiya.2200@gmail.com>
2019-05-02 13:54:07 +0900

    Finish Tutorial 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed; // 動く速さ
+    public Text scoreText; // スコアの UI
+    public Text winText; // リザルトの UI
 
     private Rigidbody rb; // Rididbody
+    private int score; // スコア
 
     void Start()
     {
         // Rigidbody を取得
         rb = GetComponent<Rigidbody>();
+
+        // UI を初期化
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void Update()
@@ -33,6 +42,26 @@ public class PlayerController : MonoBehaviour
         {
             // その収集アイテムを非表示にします
             other.gameObject.SetActive(false);
+
+            // スコアを加算します
+            score = score + 1;
+
+            // UI の表示を更新します
+            SetCountText ();
+        }
+    }
+
+    // UI の表示を更新する
+    void SetCountText()
+    {
+        // スコアの表示を更新
+        scoreText.text = "Count: " + score.ToString();
+
+        // すべての収集アイテムを獲得した場合
+        if (score >= 12)
+        {
+            // リザルトの表示を更新
+            winText.text = "You Win!";
         }
     }
 }
